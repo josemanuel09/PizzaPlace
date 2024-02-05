e69afa61c859411eba34af9191bcee7a8b126a06
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-05 10:28:12 -0400

    Creando el modelo Estados

---
+++ PizzaPlace/Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using PizzaPlace.Menu;
+
+namespace PizzaPlace.Models
+{
+    public class Estados
+    {
+        public MenuLocal Menu { get; set; } = new MenuLocal();
+        public Cesta cestaD { get; } = new Cesta();
+        public InterfazUsuario Interfaz { get; set; } = new InterfazUsuario();
+        public decimal PrecioTotal => cestaD.Pedidos.Sum(Id => Menu.ObtenerPizzas(Id)!.Precio);
+    }
+}
