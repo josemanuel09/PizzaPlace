fbe50a20969d3d049838b09c7988dc806e2c4e04
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-05 10:28:51 -0400

    Creando el modelo Cesta

---
+++ PizzaPlace/Models/Cesta.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaPlace.Models
+{
+    public class Cesta
+    {
+        public Clientes clientes { get; set; } = new Clientes();
+        public List<int> Pedidos { get; set; } = new List<int>();
+        public bool Pago { get; set; }
+        public void Add(int PizzaId) => Pedidos.Add(PizzaId);
+        public void EliminarEn(int PedidaId) => Pedidos.RemoveAt(PedidaId);
+    }
+}
