1ff2d74580113ea07e26943f0775376fcfec2a55
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-05 10:27:11 -0400

    Creando el modelo de pizzas

---
+++ PizzaPlace/Models/Pizzas.cs
@@ -0,0 +1,19 @@
+﻿namespace PizzaPlace.Models
+{
+    public class Pizzas
+    {
+        public int Id { get; }
+        public string Nombre { get; }
+        public string Despricion { get; }
+        public decimal Precio { get; }
+        public Picantes Picante { get; }
+        public Pizzas(int Id, string Nombre, string Descricpion, decimal Precio, Picantes Picantes)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.Despricion = Descricpion;
+            this.Precio = Precio;
+            this.Picante = Picantes;
+        }
+    }
+}
