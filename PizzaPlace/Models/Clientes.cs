29ecdc1b355c7df35e8e6d8f550b40b9b6036f11
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-05 10:28:27 -0400

    Creando el modelo Clientes

---
+++ PizzaPlace/Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Proporcione su nombre")]
+        public string Nombre { get; set; } = default!;
+        [Required(ErrorMessage = "Ingrese una calle con el numero de la casa")]
+        public string Calle { get; set; } = default!;
+        [Required(ErrorMessage = "Ingrese una ciudad")]
+        public string Ciudad { get; set; } = default!;
+    }
+}
