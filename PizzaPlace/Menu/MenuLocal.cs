8676b98214a2a2590c60683f5b5c921b3b124f5b
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-05 10:29:30 -0400

    Creando la carpeta Menu y dentro de ella la clase del menu local

---
+++ PizzaPlace/Menu/MenuLocal.cs
@@ -0,0 +1,11 @@
+﻿using PizzaPlace.Models;
+
+namespace PizzaPlace.Menu
+{
+    public class MenuLocal
+    {
+        public List<Pizzas> Pizzas { get; set; } = new List<Pizzas>();
+        public void Add(Pizzas pizzas) => Pizzas.Add(pizzas);
+        public Pizzas? ObtenerPizzas(int id) => Pizzas.SingleOrDefault(p => p.Id == id);
+    }
+}
