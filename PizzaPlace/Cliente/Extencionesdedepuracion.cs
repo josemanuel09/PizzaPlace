889a0c53535e695c3422409f82dded030861e398
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-05 10:29:51 -0400

    Creando las extensiones de depuracion

---
+++ PizzaPlace/Cliente/Extencionesdedepuracion.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace PizzaPlace.Cliente
+{
+    public static class Extencionesdedepuracion
+    {
+        private static JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+
+        public static string ToJson(this object obj) => JsonSerializer.Serialize(obj, options);
+    }
+}
